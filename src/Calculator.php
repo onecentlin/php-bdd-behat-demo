ab23a35104bebf7ae83ac37fc9581302c043743f
Winnie Lin <onecentlin@gmail.com>
2016-07-10 17:13:34 +0800

    GREEN: meet press add for calculation

--- src/Calculator.php
+++ src/Calculator.php
@@ -4,14 +4,25 @@ namespace Ocp;
 class Calculator
 {
     private $numbers;
+    private $total;
 
     function __construct()
     {
         $this->numbers = [];
+        $this->total = 0;
     }
 
     public function addNumber($number)
     {
         array_push($this->numbers, $number);
+    }
+
+    public function doCalculation()
+    {
+        $this->total = 0;
+        foreach($this->numbers as $number)
+        {
+            $this->total += $number;
+        }
     }
 }
