0e4ed3803e4d34afe57d0989e9f19fc34ba1c7de
Winnie Lin <onecentlin@gmail.com>
2016-07-10 17:10:41 +0800

    GREEN: meet entering numbers steps

--- src/Calculator.php
+++ src/Calculator.php
@@ -3,8 +3,15 @@ namespace Ocp;
 
 class Calculator
 {
+    private $numbers;
+
     function __construct()
     {
+        $this->numbers = [];
+    }
 
+    public function addNumber($number)
+    {
+        array_push($this->numbers, $number);
     }
 }
